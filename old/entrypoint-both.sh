b82e80f861902578c96b2f5257ca4ce391e7bbf7
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2021-12-25 22:31:15 +0100

    Create entrypoint-both.sh

---
+++ old/entrypoint-both.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sslh -f -u root --listen $LISTEN_IP:$LISTEN_PORT \
+   --ssh $SSH_HOST:$SSH_PORT \
+   --tls $HTTPS_HOST:$HTTPS_PORT \
+   --http $HTTP_HOST:$HTTP_PORT \
+   --openvpn $OPENVPN_HOST:$OPENVPN_PORT
