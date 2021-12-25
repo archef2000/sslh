bf075b0845b2ca5c049dc410e7ec3c5242c60d63
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2021-12-25 21:37:53 +0100

    Update entrypoint.sh

--- http/entrypoint.sh
+++ http/entrypoint.sh
@@ -2,5 +2,5 @@
 
 sslh -f -u root --listen=$LISTEN_IP:$LISTEN_PORT \
    --ssh=$SSH_HOST:$SSH_PORT \
-   --http=$HTTP_HOST:$HTTP_PORT \  
-   --openvpn=$OPENVPN_HOST:$OPENVPN_PORT
+   --http=$HTTP_HOST:$HTTP_PORT # \  
+#   --openvpn=$OPENVPN_HOST:$OPENVPN_PORT
