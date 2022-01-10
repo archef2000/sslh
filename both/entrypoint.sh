c5ce4bc2e349cd28c4c78116ba57f32ff1bf397f
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-01-10 22:01:50 +0100

    Delete both directory

--- both/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sslh -f -u root --listen $LISTEN_IP:$LISTEN_PORT --listen $LISTEN2_IP:$LISTEN2_PORT --ssh $SSH_HOST:$SSH_PORT --tls $HTTPS_HOST:$HTTPS_PORT --http $HTTP_HOST:$HTTP_PORT --openvpn $OPENVPN_HOST:$OPENVPN_PORT
