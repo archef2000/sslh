78df2eb18dee481c12c7e6deaec9e95d29abf50b
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-04-11 10:40:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ SSLH_OPTS=
 [ ! -z "${OPENVPN_HOST}" ] && SSLH_OPTS="${SSLH_OPTS} --openvpn ${OPENVPN_HOST}:${OPENVPN_PORT:=1194}"
 [ ! -z "${SHADOWSOCKS_HOST}" ] && SSLH_OPTS="${SSLH_OPTS} --anyprot ${SHADOWSOCKS_HOST}:${SHADOWSOCKS_PORT:=8388}"
 [ ! -z "${HTTP_HOST}" ] && SSLH_OPTS="${SSLH_OPTS} --socks5 ${SOCKS5_HOST}:${SOCKS5_PORT:=1080}"
+[ "${TRANSPARENT:=false}" = "true" ] && SSLH_OPTS="${SSLH_OPTS} --transparent"
 
 sslh -f -u root --listen 0.0.0.0:80 --listen 0.0.0.0:443 \
    ${SSLH_OPTS} \
